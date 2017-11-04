41a30d2b93ac8374e9759377628c1d5725e75740
MOONJOOYOUNG <answn3475@naver.com.com>
2017-11-04 11:49:50 +0900

    first commit

--- kakaobot/views.py
+++ kakaobot/views.py
@@ -38,7 +38,7 @@ def on_message(request):
         response = functions.Dormitory()
 
     elif content.startswith('맛집'):
-        response = "오늘의 근처 맛집 리스트 입니다.\n" + functions.FoodList()
+        response = "오늘의 근처 맛집 리스트 입니다.\n {}".format(functions.FoodList())
 
     elif content.startswith('추천'):
         response = "오늘의 추천 식당은 " + functions.NearCampus() + " 입니다."
