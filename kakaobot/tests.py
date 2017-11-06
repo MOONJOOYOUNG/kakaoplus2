be01eea902505ed77b1a6c2544b2ab44c54e0f30
MOONJOOYOUNG <answn3475@naver.com.com>
2017-11-07 02:29:43 +0900

    first commit

--- kakaobot/tests.py
+++ kakaobot/tests.py
@@ -1,4 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+utcnow = datetime.datetime.utcnow()
+time_gap = datetime.timedelta(hours=9)
+kor_time = utcnow + time_gap
 
+print(kor_time)
+
+now = datetime.datetime.now()
+nowdate = now.strftime('%m-%d')
+messages = ''
+
+t = ['월', '화', '수', '목', '금', '토', '일']
+r = kor_time.weekday()
+
+print(t[r])
