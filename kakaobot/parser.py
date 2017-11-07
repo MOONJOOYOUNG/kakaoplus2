181d4dacee36f964f283f731f5054392b15583f0
MOONJOOYOUNG <answn3475@naver.com.com>
2017-11-08 01:07:10 +0900

    first commit

--- kakaobot/parser.py
+++ kakaobot/parser.py
@@ -2,7 +2,9 @@ import re
 
 def ser(string):
     parser = string.replace('(부대햄-돈육:국내산/돈육:국내산,우육:호주산)',' ')
+    parser = string.replace('(스모크햄,후랑크햄-계육,돈육:국내산,김치-배추,고춧가루:중국산,두부-콩:외국산)', ' ')
     parser = parser.replace('(카레용돈육:덴마크산,오징어:페루산)', ' ')
+    parser = parser.replace('(카레용돈육: 덴마크산, 탕수육 - 돈육: 국내산)', ' ')
     parser = parser.replace('(부대햄-돈육:국내산/돈육:국내산,우육:호주산) ', ' ')
     parser = parser.replace('(돈육:국내산,김치-배추,고춧가루:중국산)', ' ')
     parser = parser.replace('(계육:국내산,김치-배추,고춧가루:중국산)', ' ')
@@ -17,6 +19,7 @@ def ser(string):
     parser = parser.replace('(두부-콩:외국산)', ' ')
     parser = parser.replace('(카레용돈육:덴마크산)', ' ')
     parser = parser.replace('(돈육:덴마크산)', ' ')
+    parser = parser.replace('(돈육:국내산)', ' ')
     parser = parser.replace('(흑미:국내산)', ' ')
 
 
@@ -29,6 +32,7 @@ def dong(string):
     parser = string.replace('부대햄-돈육:국내산/돈육:국내산,우육:호주산스모크햄,후랑크햄-계육,돈육:국내산김치-배추,고춧가루:중국산,사골육수-쇠고기:호주산두부-콩:외국산', ' ')
     parser = parser.replace('부대햄-돈육:국내산/돈육:국내산,우육:호주산', ' ')
 
+    parser = parser.replace('김치-배추,고춧가루:중국산', ' ')
     parser = parser.replace('돈육:국내산,우육:호주산', ' ')
     parser = parser.replace('사골육수-쇠고기:호주산 ', ' ')
     parser = parser.replace('스모크햄-계육,돈육:국내산', ' ')
