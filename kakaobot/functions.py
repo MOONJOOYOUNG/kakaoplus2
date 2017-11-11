2d3eec89ab0d2d65dce653853b357560394cad31
MOONJOOYOUNG <answn3475@naver.com.com>
2017-11-11 19:38:03 +0900

    first commit

--- kakaobot/functions.py
+++ kakaobot/functions.py
@@ -344,17 +344,17 @@ def melon_search(query):
         'query': query,
     }
     jsonp_string = requests.get('http://www.melon.com/search/keyword/index.json', params=params).text
-    json_string = jsonp_string.replace('_(','').replace(');','')
+    json_string = jsonp_string.replace('_(', '').replace(');', '')
     meta = json.loads(json_string)
-
     messages = []
+
     if 'SONGCONTENTS' in meta:
         for song in meta['SONGCONTENTS']:
-            messages.append('''[{ALBUMNAME}] {SONGNAME} by {ARTISTNAME} - http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
+            messages.append('''[{ALBUMNAME}] {SONGNAME} by {ARTISTNAME}
+   - http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
 
-            if messages:
-                message = '\n'.join(messages)
-            else:
-                message = '검색어 "{}"에 대한 노래 검색결과가 없습니다.'.format(query)
-
-            return message
+    if messages:
+        message = '\n'.join(messages)
+    else:
+        message = '검색어 "{}"에 대한 노래 검색결과가 없습니다.'.format(query)
+    return message
