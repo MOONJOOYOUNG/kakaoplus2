7da1fcef08069c184a4eb9af397a6611a9ab95e0
MOONJOOYOUNG <answn3475@naver.com.com>
2017-11-17 12:29:22 +0900

    first commit

--- kakaobot/parser.py
+++ kakaobot/parser.py
@@ -3,6 +3,8 @@ import re
 def ser(string):
     parser = string.replace('(부대햄-돈육:국내산/돈육:국내산,우육:호주산,스모크햄,후랑크햄-계육,돈육:국내산,김치-배추,고춧가루:중국산,두부-콩:외국산,사골육수-쇠고기:호주산)', ' ')
     parser = parser.replace('(부대햄-돈육:국내산/돈육:국내산,우육:호주산)',' ')
+    parser = parser.replace('(김치-배추,고춧가루:중국산,미트볼-돈육,계육:국내산)', ' ')
+    parser = parser.replace('(김치-배추,고춧가루:중국산,돈육:덴마크산)', ' ')
     parser = parser.replace('(스모크햄,후랑크햄-계육,돈육:국내산,김치-배추,고춧가루:중국산,두부-콩:외국산)', ' ')
     parser = parser.replace('(카레용돈육:덴마크산,오징어:페루산)', ' ')
     parser = parser.replace('(카레용돈육:덴마크산,탕수육-돈육:국내산)', ' ')
@@ -61,6 +63,7 @@ def dong(string):
 
 def kik(string):
     parser = string.replace('(돈,계육:국내산)', ' ')
+    parser = parser.replace('배추:중국산,고추분:중국산', ' ')
     parser = parser.replace('(배추:중국산,고추분:중국산)', ' ')
     parser = parser.replace('(배추:국내산,고추분:중국산)', ' ')
     parser = parser.replace('(배추:국내산,고추분:중국산', ' ')
@@ -77,15 +80,18 @@ def kik(string):
     parser = parser.replace('돈육:덴마크산', ' ')
     parser = parser.replace('(돈,계육:국내산)', ' ')
     parser = parser.replace('(우육:호주산)', ' ')
+    parser = parser.replace('우육:호주산', ' ')
     parser = parser.replace('(우육,육수:호주산)', ' ')
     parser = parser.replace('(육수:호주산)', ' ')
     parser = parser.replace('(우민찌;호주산)', ' ')
+    parser = parser.replace('오징어:페루산,대두:외국산', ' ')
     parser = parser.replace('(오징어:페루산)', ' ')
     parser = parser.replace('(오징어:국내산)', ' ')
+    parser = parser.replace('오징어:페루산', ' ')
     parser = parser.replace('계육:국내산', ' ')
     parser = parser.replace('(대구:네덜란드산)', ' ')
     parser = parser.replace('(가자미:미국산)', ' ')
-
+    parser = parser.replace('(두부:외국산)', ' ')
     parser = parser.replace('(대두:외국산)', ' ')
 
     parser = parser.replace('[DIY코너]', '\n[DIY코너]')
