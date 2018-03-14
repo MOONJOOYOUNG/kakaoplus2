ed5b492e7943f9e4f03bab6d46b560ed4ce81412
MOONJOOYOUNG <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-03-14 11:49:40 +0900

    Add files via upload

--- kakaobot/functions.py
+++ kakaobot/functions.py
@@ -26,10 +26,11 @@ def Library_seat():
     time.sleep(1)
     tag_id = driver.find_element_by_id('userid2')
     tag_id.send_keys("18510068")
+    time.sleep(1)
     tag_pw = driver.find_element_by_id('password2')
     tag_pw.send_keys("answndud12#")
-    tag_id.submit()
     time.sleep(1)
+    tag_id.submit()
     driver.get('https://library.seoultech.ac.kr/#/smuf/seat/status')
     time.sleep(1)
     html = driver.page_source
