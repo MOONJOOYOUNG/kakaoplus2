b74440fc2043aa980a21f9867ed08e5c69b73872
MOONJOOYOUNG <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-03-16 18:28:46 +0900

    Update functions.py

--- kakaobot/functions.py
+++ kakaobot/functions.py
@@ -33,8 +33,9 @@ def Library_seat():
         tag_id.submit()
         time.sleep(1)
     driver2.get('https://library.seoultech.ac.kr/#/smuf/seat/status')
-    time.sleep(2)
+    time.sleep(1)
     html = driver2.page_source
+    time.sleep(2)
     soup = BeautifulSoup(html, 'html.parser')
     seat = soup.select('.ikc-main span')
 
