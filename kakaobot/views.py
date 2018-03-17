c94fde60963cd72242da800f371303167ce03a16
MOONJOOYOUNG <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-03-17 18:21:13 +0900

    Update views.py

--- kakaobot/views.py
+++ kakaobot/views.py
@@ -8,9 +8,15 @@ from . import functions
 
 @bot
 def on_init(request):
-    return {'type' : 'buttons',
+    return {
+        'message': {
+            'text': response,
+        },
+        'keyboard': {
+            'type': 'buttons',
             'buttons' : ['KB 학사 학식 메뉴','성림 학사 학식 메뉴', '수림 학사 학식 메뉴','제2 학생 식당 메뉴','내일 제2 학생 식당 메뉴'
-                ,'도서관 열람실 좌석 현황','명령어']
+                , '도서관 열람실 좌석 현황', '명령어']
+        }
  }
 
 @bot
@@ -20,11 +26,11 @@ def on_message(request):
     content = request.JSON['content']  # photo 타입일 경우에는 이미지 URL
 
     if content.startswith('명령어'):
-        response = '명령어는 " " 안에 있는 단어를 입력 하시면 됩니다. ex) -> 수림\n●명령어 리스트● \n' \
+        response = ('명령어는 " " 안에 있는 단어를 입력 하시면 됩니다. ex) -> 수림\n●명령어 리스트● \n' \
                    '1.이번주 학식 메뉴 \n"케이비, 성림, 수림"\n' \
                    '2.학교 교통 노선 \n"지하철, 버스"' \
                    '\n3.네이버 실시간 검색\n"실시간,네이버"\n4.음악 검색 \n"뮤직 노래제목"' \
-                   ' ex)뮤직 박원'
+                   ' ex)뮤직 노력')
 
     elif content.startswith('도서관 열람실 좌석 현황'):
         response = functions.Library_seat()
@@ -57,32 +63,31 @@ def on_message(request):
         response = functions.Sungrim_All()
 
     elif content.startswith('지하철'):
-        response = '●7호선 공릉역 2번 출구●\n' \
+        response = ('●7호선 공릉역 2번 출구●\n' \
                    '마을버스 03번, 13번(교내 경유)\n' \
                    '●1, 6호선 석계역 1,4,6번 출구●\n' \
-                   '마을버스 03번, 13번(교내 경유)'
+                   '마을버스 03번, 13번(교내 경유)')
 
     elif content.startswith('버스'):
-        response = '●지선버스(초록버스)●\n1136번,1141번,1224번,1227번\n' \
+        response = ('●지선버스(초록버스)●\n1136번,1141번,1224번,1227번\n' \
                    '●마을버스 - 03번●\n' \
                    '●교내 경유 마을버스 - 13번●\n' \
-                   '정문-다산관-붕어방-다산관-주차로터리'
+                   '정문-다산관-붕어방-다산관-주차로터리')
 
     elif content.startswith('뮤직 '):
         query = content[3:]
-        response = '멜론 "{}" 검색결과\n\n'.format(query) + functions.melon_search(query)
+        response = ('멜론 "{}" 검색결과\n\n'.format(query) + functions.melon_search(query))
 
     elif content.startswith('실시간') | content.startswith('네이버'):
-        response = '네이버 실시간 검색어\n {}'.format(functions.naver_rank())
-
-    elif content.startswith('밥집'):
-        response = "과기대 근처 밥집 리스트 입니다.\n{}".format(functions.FoodList())
-
-    elif content.startswith('추천'):
-        response = "오늘의 추천 식당은 " + functions.NearCampus() + " 입니다."
+        response = ('네이버 실시간 검색어\n {}'.format(functions.naver_rank()))
 
     else:
-        response = '지원하는 명령어가 아닙니다. "명령어" 입력 시 지원되는 기능을 볼 수 있습니다.'
+        response = ('지원하는 명령어가 아닙니다. "명령어" 입력 시 지원되는 기능을 볼 수 있습니다.\n' \
+                   '명령어는 " " 안에 있는 단어를 입력 하시면 됩니다. ex) -> 수림\n●명령어 리스트● \n' \
+                   '1.이번주 학식 메뉴 \n"케이비, 성림, 수림"\n' \
+                   '2.학교 교통 노선 \n"지하철, 버스"' \
+                   '\n3.네이버 실시간 검색\n"실시간,네이버"\n4.음악 검색 \n"뮤직 노래제목"' \
+                   ' ex)뮤직 노력')
 
     return {
         'message': {
@@ -105,7 +110,3 @@ def on_block(request, user_key):
 @bot
 def on_leave(request, user_key):
     pass
-
-
-# Create your views here.
-
