f924533010bb6dee19ef99c95950e363417fd60b
MOONJOOYOUNG <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-03-17 20:24:42 +0900

    Update views.py

--- kakaobot/views.py
+++ kakaobot/views.py
@@ -11,9 +11,8 @@ def on_init(request):
     return {
         'type': 'text',
         'keyboard': {
-            'type': 'buttons',
-            'buttons' : ['식단','KB 학사 학식 메뉴','성림 학사 학식 메뉴', '수림 학사 학식 메뉴','제2 학생 식당 메뉴','내일 제2 학생 식당 메뉴'
-                , '도서관 열람실 좌석 현황', '명령어']
+            'type': 'buttons',      
+            'buttons' : ['오늘 식단표','이번주 식단표','도서관 열람실 좌석 현황','네이버 실시간 검색어']
         }
  }
 
@@ -29,7 +28,7 @@ def on_message(request):
                    '2.학교 교통 노선 \n"지하철, 버스"' \
                    '\n3.네이버 실시간 검색\n"실시간,네이버"\n4.음악 검색 \n"뮤직 노래제목"' \
                    ' ex)뮤직 노력')
-    elif content.startswith('식단'):
+    elif content.startswith('오늘 식단표'):
         return ({
 			'message':{
 				'text':'장소를 골라주세요.'
@@ -39,6 +38,19 @@ def on_message(request):
 				'buttons':['KB 학사 학식 메뉴','성림 학사 학식 메뉴', '수림 학사 학식 메뉴','제2 학생 식당 메뉴','내일 제2 학생 식당 메뉴']
 			}
 		})
+
+    elif content.startswith('이번주 식단표'):
+        return ({
+			'message':{
+				'text':'장소를 골라주세요.'
+			},
+			'keyboard':{
+				'type':'buttons',
+				'buttons':['케이비','성림', '수림','제2 학생 식당']
+			}
+		})
+   
+
     elif content.startswith('도서관 열람실 좌석 현황'):
         response = functions.Library_seat()
 
@@ -54,7 +66,7 @@ def on_message(request):
     elif content.startswith('제2 학생 식당 메뉴'):
         response = functions.Food_two()
 
-    elif content.startswith('내일 제2 학생 식당 메뉴'):
+    elif content.startswith('제2 학생 식당'):
         response = functions.Food_two_tomorrow()
 
     elif content.startswith('도서관 열람실 좌석 현황'):
@@ -81,20 +93,11 @@ def on_message(request):
                    '●교내 경유 마을버스 - 13번●\n' \
                    '정문-다산관-붕어방-다산관-주차로터리')
 
-    elif content.startswith('뮤직 '):
-        query = content[3:]
-        response = ('멜론 "{}" 검색결과\n\n'.format(query) + functions.melon_search(query))
-
-    elif content.startswith('실시간') | content.startswith('네이버'):
+    elif content.startswith('네이버 실시간 검색어'):
         response = ('네이버 실시간 검색어\n {}'.format(functions.naver_rank()))
 
     else:
-        response = ('지원하는 명령어가 아닙니다. "명령어" 입력 시 지원되는 기능을 볼 수 있습니다.\n' \
-                   '명령어는 " " 안에 있는 단어를 입력 하시면 됩니다. ex) -> 수림\n●명령어 리스트● \n' \
-                   '1.이번주 학식 메뉴 \n"케이비, 성림, 수림"\n' \
-                   '2.학교 교통 노선 \n"지하철, 버스"' \
-                   '\n3.네이버 실시간 검색\n"실시간,네이버"\n4.음악 검색 \n"뮤직 노래제목"' \
-                   ' ex)뮤직 노력')
+        response = ('지원하는 명령어가 아닙니다.')
 
     return {
         'message': {
@@ -102,8 +105,7 @@ def on_message(request):
         },
         'keyboard': {
             'type': 'buttons',
-            'buttons' : ['식단','KB 학사 학식 메뉴','성림 학사 학식 메뉴', '수림 학사 학식 메뉴','제2 학생 식당 메뉴','내일 제2 학생 식당 메뉴'
-                , '도서관 열람실 좌석 현황', '명령어']
+            'buttons' : 'buttons' : ['오늘 식단표','이번주 식단표','도서관 열람실 좌석 현황','네이버 실시간 검색어']
         }
     }
 
