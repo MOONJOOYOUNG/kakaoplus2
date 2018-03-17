7266f467f78168be26398b0c8baf26292024410d
MOONJOOYOUNG <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-03-17 20:25:07 +0900

    Update functions.py

--- kakaobot/functions.py
+++ kakaobot/functions.py
@@ -349,30 +349,3 @@ def naver_rank():
     for idx, tag in enumerate(tag_list, 1):
         message.append(str(idx) + '.' + tag.text)
     return message
-
-# 멜론 음악 검색.
-def melon_search(query):
-    params = {
-        'jscallback': '_',
-        'query': query,
-    }
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.167 Safari/537.36',
-    }
-    jsonp_string = requests.get('http://www.melon.com/search/keyword/index.json', headers=headers, params=params).text
-    json_string = jsonp_string.replace('_(', '').replace(');', '')
-    meta = json.loads(json_string)
-    messages = []
-
-    if 'SONGCONTENTS' in meta:
-        for song in meta['SONGCONTENTS']:
-            messages.append('''[{ALBUMNAME}] {SONGNAME} by {ARTISTNAME}
-   - http://www.melon.com/song/detail.htm?songId={SONGID}'''.format(**song))
-
-    if messages:
-        message = '\n'.join(messages)
-    else:
-        message = '검색어 "{}"에 대한 노래 검색결과가 없습니다.'.format(query)
-    return message
-
-
