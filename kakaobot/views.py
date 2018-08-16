b5f9b8842a674d1cfd9a7b17fc98827428dbaf36
문주영 <33341029+MOONJOOYOUNG@users.noreply.github.com>
2018-08-16 18:35:13 +0900

    Update views.py

--- kakaobot/views.py
+++ kakaobot/views.py
@@ -53,7 +53,7 @@ def on_message(request):
 			},
 			'keyboard':{
 				'type':'buttons',
-				'buttons':['공릉역 2번 출구', '과기대 붕어방','과기대 정문','정문(GS 편의점)','과기대 정문 13번']
+				'buttons':['공릉역 2번 출구', '과기대 붕어방','과기대 정문','정문(GS 편의점)','정문 13번 공릉방향']
 			}
 		})   
 
@@ -102,7 +102,7 @@ def on_message(request):
     elif content.startswith('정문(GS 편의점)'):
         response = functions.front_door_gs()
 	
-    elif content.startswith('과기대 정문 13번'):
+    elif content.startswith('정문 13번 공릉방향'):
         response = functions.front_door_13()
 	
     elif content.startswith('공릉동 미세먼지'):
